5bcc1af3b8c87392bb7fd88b0a34d2fbe78a9fad
Magnus Olsson <magnus.olsson@projectplace.com>
2010-08-17 09:03:06 +0200

    Added documentation header to function.

--- file.py
+++ file.py
@@ -2,5 +2,17 @@
 A new file
 """
 
+
 def add(a, b):
+    """
+    blah blah...
+    
+    @param a:
+    @type a:
+    @param b:
+    @type b:
+    @return:
+    @returntype:
+    """
     return a + b
+
